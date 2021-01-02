1747630f2315f0245e4aef5f33eac3c226984782
Fred Borgquist <git@borgquist.com>
2021-01-02 17:24:31 +0000

    nohup added

--- rotarystart.sh
+++ rotarystart.sh
@@ -12,8 +12,8 @@ sudo chmod 755 rotarystart.sh
 echo "file permissions set"
 
 echo "starting restbutton.py"
-/usr/bin/python3 resetbutton.py &> /dev/null
+nohup /usr/bin/python3 resetbutton.py &
 
 echo "starting rotarymeds.py"
-/usr/bin/python3 rotarymeds.py &> /dev/null
+nohup /usr/bin/python3 rotarymeds.py &
 echo "rotarystart.sh complete"
