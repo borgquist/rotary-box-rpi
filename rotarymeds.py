498fe88411cab5833c26446cd0daf68036b448dc
Fred Borgquist <git@borgquist.com>
2021-01-07 15:13:51 +0000

    Update rotarymeds.py

--- rotarymeds.py
+++ rotarymeds.py
@@ -75,7 +75,7 @@ while(not haveInternet()):
 logging.info("have internet connectivity")
 
 logging.info("Creating FirebaseConnection")
-firebaseConnection = FirebaseConnection(boxState.cpuId)
+firebaseConnection = FirebaseConnection(cpuid=boxState.cpuId)
 logging.info("Done creating FirebaseConnection")
 
 def getLatestScheduleFromFirebase():
