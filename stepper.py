b88246c2f807db0cff9b96f8db3639412a719b8e
Fred Borgquist <git@borgquist.com>
2021-01-07 11:31:24 +0000

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -1,6 +1,8 @@
- class Stepper:
-        maxMove = 3000
-        minMove = 2000
-        afterTrigger = 1000
-        def __str__(self):
-            return "maxMove [" + str(self.maxMove) + "] minMove [" + str(self.minMove) + "] afterTrigger [" + str(self.afterTrigger) + "]"
+class Stepper:
+    name = "unknown"
+    maxMove = 3000
+    minMove = 2000
+    afterTrigger = 1000
+    chanList = []
+    def __str__(self):
+        return "maxMove [" + str(self.maxMove) + "] minMove [" + str(self.minMove) + "] afterTrigger [" + str(self.afterTrigger) + "]"
