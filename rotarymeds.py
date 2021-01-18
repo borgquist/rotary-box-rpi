2c199c098dcdf5b0532cf5e561c37718b2c201b4
Fred Borgquist <git@borgquist.com>
2021-01-18 21:25:55 +0000

    Update rotarymeds.py

--- rotarymeds.py
+++ rotarymeds.py
@@ -10,8 +10,8 @@ import socket  # used for hostname
 import traceback
 import subprocess
 from firebase import FirebaseConnection
-from /home/pi/rotary-box-rpi/datetimefunctions import DateTimeFunctions
-from /home/pi/rotary-box-rpi/datetimefunctions import Box
+from datetimefunctions import DateTimeFunctions
+from box import Box
 
 folderPath = '/home/pi/'
 os.makedirs(folderPath + "logs/", exist_ok=True)
