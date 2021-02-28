e7cbad5572718adc8b8dc50b9b325c814eab6800
Fred Borgquist <git@borgquist.com>
2021-02-28 13:11:02 +0000

    Update rotarymeds.py

--- rotarymeds.py
+++ rotarymeds.py
@@ -53,12 +53,12 @@ googleHostForInternetCheck = "8.8.8.8"
 
 def getserial():
     # Extract serial from cpuinfo file
-    cpuserial = "0000000000000000"
+    cpuserial = "123456789123456789"
     try:
         f = open('/proc/cpuinfo', 'r')
         for line in f:
             if line[0:6] == 'Serial':
-                cpuserial = line[10:26]
+                cpuserial = line[10:26].lstrip('0')
         f.close()
     except:
         cpuserial = "ERROR000000000"
