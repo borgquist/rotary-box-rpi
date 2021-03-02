77b60f4a8adfab13574e58066bbd4ea344c053a1
Fred Borgquist <git@borgquist.com>
2021-03-02 17:47:10 +0000

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -7,7 +7,7 @@ class Stepper:
     afterTrigger = 1000
     chanList = []
     def __str__(self):
-        return "name [" + str(self.name) + "] maxMove [" + str(self.maxMove) + "] minMove [" + str(self.minMove) + "] afterTrigger [" + str(self.afterTrigger) + "]"
+        return "name [" + str(self.name) + "] maxMove [" + str(self.maxMove) + "] minMove [" + str(self.minMove) + "] afterTrigger [" + str(self.afterTrigger) + "] chanList [" + str(self.chanList) + "]"
 
     def toJSON(self):
         return {"name": self.name,
