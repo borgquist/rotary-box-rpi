dd327ee33994429809c00beaf0770b5b42f4c649
Fred Borgquist <git@borgquist.com>
2021-03-12 17:12:15 +0000

    Update rotarymeds.py

--- rotarymeds.py
+++ rotarymeds.py
@@ -453,8 +453,6 @@ def thread_time(name):
 def getAndUpdateNextMoveFirebase(circle: BoxCircle):
     nextMove = getNextMove(circle.settings.schedules)
     nextMoveInEpoch = nextMove.timestamp()
-    logging.info("epoch nextMove is " + str(nextMoveInEpoch) + " for " + str(circle)) 
-
     if(str(nextMove) != circle.state.nextMove):
         firebaseConnection.setFirebaseValue(
             "nextMove", str(nextMove).strip(), "state", circle.name, "circles")
