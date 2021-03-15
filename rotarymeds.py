a441f436f2aeb8b3edffb4e20bee4cb156c86560
Fred Borgquist <git@borgquist.com>
2021-03-15 10:03:10 +0000

    Update rotarymeds.py

--- rotarymeds.py
+++ rotarymeds.py
@@ -23,7 +23,7 @@ import requests
 folderPath = '/home/pi/'
 os.makedirs(folderPath + "logs/", exist_ok=True)
 
-logging.basicConfig(format='%(asctime)s.%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+logging.basicConfig(format='%(asctime)s.%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] [%(funcName)s] %(message)s',
                     datefmt='%Y-%m-%d:%H:%M:%S',
                     level=logging.INFO
                     )
