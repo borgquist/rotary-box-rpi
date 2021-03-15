ea64414a6f49b4c1bb7b718788d1820b82995753
Fred Borgquist <git@borgquist.com>
2021-03-15 11:07:10 +0000

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -69,11 +69,11 @@ class FirebaseConnection:
                     logMessasge =  greatgrandparent + "/" + grandparent + "/" + logMessasge
 
 
-        # TODO this is a bit unnecessary, we're doing an extra get and it should not be needed
-        currentValue = self.getFirebaseValue(settingname, None, parent, grandparent, greatgrandparent)
-        if(currentValue == newValue):
-            logger.info("setting [" + settingname + "] value [" + str(currentValue) + "] did not need udpating but we have done a get [" + logMessasge + "]")
-            return # no need to update
+        # # TODO this is a bit unnecessary, we're doing an extra get and it should not be needed
+        # currentValue = self.getFirebaseValue(settingname, None, parent, grandparent, greatgrandparent)
+        # if(currentValue == newValue):
+        #     logger.info("setting [" + settingname + "] value [" + str(currentValue) + "] did not need udpating but we have done a get [" + logMessasge + "]")
+        #     return # no need to update
 
         logger.info("setting [" + logMessasge + "] to [" + str(newValue) + "] old value [" + str(currentValue) + "]")
 
