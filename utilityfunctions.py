a1b4cda0df8726e2c6997800158c4f345a54142c
Fred Borgquist <git@borgquist.com>
2021-03-16 14:03:19 +0000

    Update utilityfunctions.py

--- utilityfunctions.py
+++ utilityfunctions.py
@@ -6,7 +6,7 @@ class UtilityFunctions:
     
     @staticmethod
     def generateId() -> str:
-        return datetime.now().strftime('%Y%m-%d%H-%M%S-') + str(uuid4())
+        return datetime.now().strftime('%Y%m%d-%H%M%S-') + str(uuid4())
 
     @staticmethod
     def getserial() -> str:
