c51bb4f4a1415a97c24c7b5aaba2b1938871366b
Fred Borgquist <git@borgquist.com>
2021-03-21 16:44:12 +0000

    Update resetbutton.py

--- resetbutton.py
+++ resetbutton.py
@@ -9,73 +9,46 @@ import json
 
 def flashButtonLed(speedInSeconds, nrFlashes, finalValue):
     ledOn = False
-    
     def setButtonLedOn(setToOn):
         if(setToOn):
             GPIO.output(button_led_pin,GPIO.HIGH)
         else:
             GPIO.output(button_led_pin,GPIO.LOW)
-
     for x in range(int(nrFlashes)):
         ledOn = not ledOn
         setButtonLedOn(ledOn)
         time.sleep(speedInSeconds)
     setButtonLedOn(finalValue)
 
-def triggerResetMode():
-    logger.info("reset wifi triggered")
-    flashButtonLed(0.5, 6, True)
-    
-    os.system('sudo wifi-connect -s PodQ-setupWiFi')
-    logger.info("reset wifi complete")
-    
-    flashButtonLed(0.5, 10, False)
-    logger.info("calling reboot")
-    
-    os.system('sudo reboot now')
-
-def haveInternet() -> bool:
-    googleHostForInternetCheck = "8.8.8.8"
-    try:
-        output = subprocess.check_output(
-            "ping -c 1 {}".format(googleHostForInternetCheck), shell=True)
-    except Exception:
-        return False
-    return True
-
-def thread_internet_connectivity(name):
-    timestampInternetOK = 1
-    timestampLastInternetNotOk = 0
-    internetOutCount = 0
+def thread_button_flasher(name):
+    global flash_button
     while not exitapp: 
         try: 
-            
-            if(not haveInternet()):
-                if(timestampLastInternetNotOk < timestampInternetOK):
-                    internetOutCount = internetOutCount + 1
-                    logger.warning("internet is not available, outage count: " + str(internetOutCount))
+            if(flash_button):
                 flashButtonLed(0.5, 6, True) #works as a sleep too
-                timestampLastInternetNotOk = time.time()
             else:
-                if(timestampInternetOK < timestampLastInternetNotOk):
-                    logger.info("there is internet again, outage count: " + str(internetOutCount))
-                timestampInternetOK = time.time()
-                time.sleep(5)
+                time.sleep(1)
     
         except Exception as err:
             logger.error("exception " +  traceback.format_exc())
         
-    logger.info("thread_internet_connectivity    : exiting")    
+    logger.info("thread_button_flasher    : exiting")    
 
 def thread_button(name):
     timeButtonNotPressed = 0
+    global flash_button
     while not exitapp:
         try:
             if GPIO.input(button_pushed_pin) == GPIO.HIGH :
                 if(time.time() - timeButtonNotPressed > 5):
                     if(fiveSecondPressDone == False):
                         logger.info("five second press, calling wifi-connect")
-                        triggerResetMode()
+                        flash_button = True
+                        os.system('sudo wifi-connect -s PodQ-setupWiFi')
+                        logger.info("reset wifi complete, calling reboot")
+                        os.system('sudo reboot now')
+                        exitapp = True
+                        return
                     fiveSecondPressDone = True
                 else:
                     fiveSecondPressDone = False
@@ -91,6 +64,8 @@ def thread_button(name):
 if __name__ == '__main__':
     try:
         exitapp = False
+        global flash_button
+        flash_button = False
         folderPath = '/home/pi/'
         os.makedirs(folderPath + "logs/", exist_ok=True)
         logFormat = '%(asctime)s.%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] [%(funcName)s] %(message)s'
@@ -122,9 +97,9 @@ if __name__ == '__main__':
         buttonThread.start()
         logger.info("thread_button stared")
 
-        internetThread = threading.Thread(target=thread_internet_connectivity, args=(1,))
-        internetThread.start()
-        logger.info("thread_internet_connectivity stared")
+        flashButtonThread = threading.Thread(target=thread_button_flasher, args=(1,))
+        flashButtonThread.start()
+        logger.info("thread_button_flasher stared")
         logger.info("Podq wifi-connect monitor started")
         while (True):
             time.sleep(10)
