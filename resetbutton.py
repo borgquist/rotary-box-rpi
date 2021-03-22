69e9fff64c16b6f9bde40f5c439361837dbc4bc2
Fred Borgquist <git@borgquist.com>
2021-03-22 10:22:05 +0000

    Update resetbutton.py

--- resetbutton.py
+++ resetbutton.py
@@ -14,17 +14,8 @@ flash_button = False
 
 def removeNetworkConnections():
     logger.info("removing network connections")
-    # bus = dbus.SystemBus()
-    # proxy = bus.get_object("org.freedesktop.NetworkManager", "/org/freedesktop/NetworkManager/Settings")
-    # settings = dbus.Interface(proxy, "org.freedesktop.NetworkManager.Settings")
-
-    # connection_paths = settings.ListConnections()
-
-    # for path in connection_paths:
-    #     con_proxy = bus.get_object("org.freedesktop.NetworkManager", path)
-    #     settings_connection = dbus.Interface(con_proxy, "org.freedesktop.NetworkManager.Settings.Connection")
-    #     config = settings_connection.GetSettings()
-    #     settings_connection.Delete()
+    
+    os.system('sudo rm /etc/NetworkManager/system-connections/*')
     os.system('sudo python3 /home/pi/clearnetwork.py')
                         
     logger.info("done removing network connections")
