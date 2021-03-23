3bfdeddd4a86c55a07d7a23fc38c6c7e07634449
Fred Borgquist <git@borgquist.com>
2021-03-23 21:30:53 +0000

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -118,7 +118,7 @@ class FirebaseConnection:
         return str(latestVersion.val())
     
     def getBoxLatestCommandTimestamp(self) -> float:
-        latestTimestamp = self.database.child("box").child(self.cpuid).child("commands").child("latestTimestamp").get()
+        latestTimestamp = self.database.child("box").child("boxes").child(self.cpuid).child("commands").child("latestTimestamp").get()
         if latestTimestamp.val() is None:
             logger.warning("couldn't get latestTimestamp")
             return "unknown"
