041950c03f167730e120528326778a0b4749e933
Fred Borgquist <git@borgquist.com>
2021-03-27 13:45:52 +0000

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -4,3 +4,12 @@ sudo rm -r /home/pi/podq-box-rpi
 mkdir /home/pi/podq-box-rpi
 cd /home/pi/
 git clone https://github.com/borgquist/podq-box-rpi.git
+
+SERVICE="podq.py"
+if pgrep -f "$SERVICE" >/dev/null
+then
+    echo "$SERVICE is already running"
+else
+    echo "$SERVICE is stopped, starting it"
+    sh /home/pi/podq-box-rpi/podq.py
+fi
